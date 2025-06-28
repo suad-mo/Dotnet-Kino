9cfbb8c67848246686e7df83b55f0535e6dd24dc
suad-krvavac <suad.krvavac@gmail.com>
2025-06-28 16:18:58 +0200

    Update Kino.Services

---
+++ Kino.Services/Artikl/ArtiklService.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Kino.Core.Pagination;
+using Kino.Core.ViewModels;
+using Kino.Repositories.Artikl;
+
+namespace Kino.Services.Artikl
+{
+    public class ArtiklService : IArtiklService
+    {
+        private readonly IArtiklRepository _artiklRepository;
+        private readonly IMapper _mapper;
+
+        public ArtiklService(IArtiklRepository artiklRepository, IMapper mapper)
+        {
+            _artiklRepository = artiklRepository;
+            _mapper = mapper;
+        }
+        public ArtiklViewModel Delete(int id)
+        {
+            var artikl = _artiklRepository.Delete(id);
+            if (artikl == null)
+                return null;
+
+            return _mapper.Map<ArtiklViewModel>(artikl);
+        }
+
+        public List<ArtiklViewModel> GetArtikle()
+        {
+            var artikl = _artiklRepository.GetAll();
+            if (artikl == null || artikl.ToArray().Length == 0)
+                return null;
+
+            return _mapper.Map<List<ArtiklViewModel>>(artikl);
+        }
+
+        public ArtiklViewModel GetOneArtikl(int id)
+        {
+            var artikl = _artiklRepository.GetOne(id);
+            if (artikl == null)
+                return null;
+
+            return _mapper.Map<ArtiklViewModel>(artikl);
+        }
+
+        public ArtiklViewModel Save(ArtiklViewModel artiklViewModel)
+        {
+            var artikl = _artiklRepository.Save(artiklViewModel);
+            if (artikl == null)
+                return null;
+
+            return _mapper.Map<ArtiklViewModel>(artikl);
+        }
+
+        public List<ArtiklViewModel> GetAllArtikle(int pageNumber, int pageSize, string searchType, string searchQuery)
+        {
+            var artiklList = _artiklRepository.GetArtikle(pageNumber, pageSize, searchType, searchQuery);
+
+            return artiklList;
+        }
+
+        public PaginationMetadata GetPaginationMetadata(int pageSize)
+        {
+            var count = _artiklRepository.GetArtiklCount();
+
+            var pageCount = (int)Math.Ceiling(count / (double)pageSize);
+
+            return new PaginationMetadata(count, pageCount);
+        }
+    }
+}
---
+++ Kino.Services/Artikl/IArtiklService.cs
@@ -0,0 +1,15 @@
+﻿using Kino.Core.Pagination;
+using Kino.Core.ViewModels;
+
+namespace Kino.Services.Artikl
+{
+    public interface IArtiklService
+    {
+        List<ArtiklViewModel> GetAllArtikle(int pageNumber, int pageSize, string searchType, string searchQuery);
+        List<ArtiklViewModel> GetArtikle();
+        ArtiklViewModel GetOneArtikl(int id);
+        ArtiklViewModel Save(ArtiklViewModel artiklViewModel);
+        ArtiklViewModel Delete(int id);
+        PaginationMetadata GetPaginationMetadata(int pageSize);
+    }
+}
